e0ca77580b3c8c125479e6fc3b5f04d166ccae7f
Bruno Ruiz <brunoruizmachado@gmail.com>
2025-05-04 17:41:56 -0300

    feat: adiciona estrutura inicial do login backend

---
+++ Login_PI/Login_PI/Controllers/LoginContoller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login_PI.Controllers
+{
+    [ApiController]
+    [Route("[controller")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {   // Usuario defult por enquanto esperando o BD
+            if(request.Username == "admin" && request.Password == "1234")
+            {
+                return Ok(new { message = "Login realizado com sucesso!" });
+
+            }
+            return Unauthorized(new { message = "Usuario ou senha inválidos." });
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
